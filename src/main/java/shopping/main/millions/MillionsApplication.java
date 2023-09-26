778d8b5dc59f47ef73a379803746a90ce91c3c3a
KimDaeHan <daehan128@gmail.com>
2023-09-26 22:31:58 +0900

    commit

---
+++ src/main/java/shopping/main/millions/MillionsApplication.java
@@ -0,0 +1,13 @@
+package shopping.main.millions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MillionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shopping/main/millions/MillionsApplicationTests.java
@@ -0,0 +1,13 @@
+package shopping.main.millions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

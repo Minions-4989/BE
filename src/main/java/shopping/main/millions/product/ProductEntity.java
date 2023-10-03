6ba3a5f0964ea58c2dbce11e7cd7ce5b67bf1247
sojinb <thwls3791@naver.com>
2023-10-03 14:47:48 +0900

    refactor: Entity Annotation 추가

--- src/main/java/shopping/main/millions/product/ProductEntity.java
+++ src/main/java/shopping/main/millions/product/ProductEntity.java
@@ -5,14 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @NoArgsConstructor
+@Entity
 @Table(name = "product")
 public class ProductEntity {
 

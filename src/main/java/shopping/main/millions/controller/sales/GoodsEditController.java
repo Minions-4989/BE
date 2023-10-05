86de4d93d3c3627d12a839657fee3bca618e1e22
KimDaeHan <daehan128@gmail.com>
2023-10-05 20:32:13 +0900

    refactor: Controller 수정

--- src/main/java/shopping/main/millions/controller/sales/GoodsEditController.java
+++ src/main/java/shopping/main/millions/controller/sales/GoodsEditController.java
@@ -5,18 +5,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shopping.main.millions.dto.sales.GoodsSaveDto;
 import shopping.main.millions.service.sales.GoodsSaveService;
 
 import java.util.Map;
 @Log4j2
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RestController("/goods")
+@RequestMapping("/goods")
 public class GoodsEditController {
 
     private final GoodsSaveService goodsSaveService;

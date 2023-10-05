17468da682cdf8d6760caa77334ce0d09adef262
sojinb <thwls3791@naver.com>
2023-10-05 21:03:51 +0900

    feat: 장바구니 담기 추가

--- src/main/java/shopping/main/millions/controller/cart/CartController.java
+++ src/main/java/shopping/main/millions/controller/cart/CartController.java
@@ -2,11 +2,18 @@ package shopping.main.millions.controller.cart;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import shopping.main.millions.dto.cart.CartProductDto;
 import shopping.main.millions.service.cart.CartService;
 
+import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.Map;
 
 @Log4j2
@@ -18,11 +25,11 @@ public class CartController {
     private final CartService cartService;
 
     //상품 장바구니에 추가
-//    @PostMapping("/add")
-//    public ResponseEntity<Map<String,String>> addProductToCart(@RequestBody CartProductDto cartProductDto){
-//
-//        return cartService.addCart(cartProductDto,);
-//    }
+    @PostMapping("/add")
+    public ResponseEntity<Map<String,String>> addProductToCart(@RequestBody CartProductDto cartProductDto){
+
+        return cartService.addCart(cartProductDto);
+    }
 
     //장바구니 조회
     @GetMapping("/")

b09b9f3ff5a4408d5e7cbaaf04b668b6d8148ff9
sojinb <thwls3791@naver.com>
2023-10-05 14:40:08 +0900

    feat: 장바구니 로직 구현중

--- src/main/java/shopping/main/millions/controller/cart/CartController.java
+++ src/main/java/shopping/main/millions/controller/cart/CartController.java
@@ -2,11 +2,31 @@ package shopping.main.millions.controller.cart;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shopping.main.millions.dto.cart.CartProductDto;
+import shopping.main.millions.service.cart.CartService;
+
+import java.util.Map;
+
 @Log4j2
 @RestController
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
+
+    private final CartService cartService;
+
+    //상품 장바구니에 추가
+//    @PostMapping("/add")
+//    public ResponseEntity<Map<String,String>> addProductToCart(@RequestBody CartProductDto cartProductDto){
+//
+//        return cartService.addCart(cartProductDto,);
+//    }
+
+    //장바구니 조회
+    @GetMapping("/")
+    public ResponseEntity<CartProductDto> viewCart(){
+        return  null;
+    }
 }

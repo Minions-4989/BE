b0f044c8937f0abba3e9cd85e53d369aa796fcd9
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-06 13:01:54 +0900

    feat: OptionDto 생성

---
+++ src/main/java/shopping/main/millions/dto/cart/OptionDto.java
@@ -0,0 +1,15 @@
+package shopping.main.millions.dto.cart;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OptionDto {
+
+        private String productSize; // 옵션사이즈
+        private String productColor; // 옵션컬러
+        private Long productCount; // 구매할개수
+
+}

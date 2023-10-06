80252e5c5afd5ee038df6bebb24edd203a533988
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-10-06 20:39:55 +0900

    refactor: GoodsImageDto 수정

--- src/main/java/shopping/main/millions/dto/sales/GoodsImageDto.java
+++ src/main/java/shopping/main/millions/dto/sales/GoodsImageDto.java
@@ -5,25 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import shopping.main.millions.entity.product.ProductEntity;
 
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
-@Getter @Setter
+@Getter
+@Setter
+@SuperBuilder
 @NoArgsConstructor
 public class GoodsImageDto {
-
-    private Long imageId;
-
-    private Long productId;
-
-    public GoodsImageDto toImageEntity(){
-        return GoodsImageDto.builder()
-                .imageId(this.imageId)
-                .build();
-    }
-    @Builder
-    public GoodsImageDto(Long imageId, Long productId) {
-        this.imageId = imageId;
-        this.productId = productId;
-    }
+    private String productImage;
+    private String productImageOriginName;
+    private String productImageSave;
+    private ProductEntity productEntity;
 }

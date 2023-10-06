f7714eb29e1b57cd68d76f3d38f46040d483968b
KimDaeHan <daehan128@gmail.com>
2023-10-06 21:44:12 +0900

    feat: create CategoryDto

---
+++ src/main/java/shopping/main/millions/dto/sales/CategoryDto.java
@@ -0,0 +1,14 @@
+package shopping.main.millions.dto.sales;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDto {
+    private String categoryName;
+}

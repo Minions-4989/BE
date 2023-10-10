d36b82395268306b2a2064856a0caacc58976864
sojinb <thwls3791@naver.com>
2023-10-10 17:13:20 +0900

    feat: 주문엔티티 생성

---
+++ src/main/java/shopping/main/millions/entity/order/OrderPaymentEntity.java
@@ -0,0 +1,39 @@
+package shopping.main.millions.entity.order;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+@Table(name = "order_payment")
+public class OrderPaymentEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_payment_id")
+    private Long orderPaymentId; // 주문 번호
+
+    @Column(name = "card_num")
+    private String cardNum; // 카드 번호
+
+    @Column(name = "card_cvc")
+    private String cardCvc; // 카드 cvc
+
+    @Column(name = "card_expiration_period")
+    private String cardExpirationPeriod; //카드 유효기간
+
+    @Column(name = "delivery_price")
+    private String deliveryPrice; // 결제 총 금액
+
+    @Column(name = "delivery_date")
+    private LocalDateTime deliveryDate; // 구매 날짜
+
+    @ManyToOne
+    @JoinColumn(name = "user_order_id")
+    private UserOrderEntity userOrderEntity;
+}

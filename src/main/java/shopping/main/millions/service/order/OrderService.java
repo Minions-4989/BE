d719e935d396d0d9a2522cd08eb8094041ab67e9
sojinb <thwls3791@naver.com>
2023-10-11 16:50:20 +0900

    feat: OrderService 로직 구현

--- src/main/java/shopping/main/millions/service/order/OrderService.java
+++ src/main/java/shopping/main/millions/service/order/OrderService.java
@@ -8,4 +8,46 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final UserOrderRepository userOrderRepository;
+    private final OrderPaymentRepository orderPaymentRepository;
+
+    // 다른서비스가 아니고 그 저희 ENtity가 총 3개 있자나여 쟤네를 저장하는 메소드를 각각 만드는게 어떨까 싶어서요. 총 3개
+    public ResponseEntity<?> saveUser(OrderDto orderDto) {
+        // Entity 변환 후 저장
+        UserOrderEntity userOrderEntity = UserOrderEntity.builder()
+                .telNumber(orderDto.getTelNumber())
+                .userName(orderDto.getUserName())
+                .address(orderDto.getAddress())
+                .addressDetail(orderDto.getAddressDetail())
+                .addressZipcode(orderDto.getAddressZipcode())
+                .build();
+        //JPA의 JpaRepository 매개변수, 인터페이스 명이 일치한 지 확인하면 된다. bb
+        userOrderRepository.save(userOrderEntity);
+
+        return ResponseEntity.ok("저장 완료");
+    }
+
+    public ResponseEntity<?> saveUserPayment(OrderDto orderDto){
+        OrderPaymentEntity orderPaymentEntity = OrderPaymentEntity.builder()
+                .cardNum(orderDto.getCardNum())
+                .cardCvc(orderDto.getCardCvc())
+                .cardExpirationPeriod(orderDto.getCardExpirationPeriod())
+                .totalPrice(orderDto.getTotalPrice())
+                .build();
+        orderPaymentRepository.save(orderPaymentEntity);
+        return ResponseEntity.ok("저장 완료");
+
+    }
+
+//    public ResponseEntity<?> saveOrder(OrderDto orderDto){
+//        OrderEntity orderEntity = OrderEntity.builder()
+//                .productEntity()
+//                .build();
+//    }
+
+
 }
+//근데 준용님 이거 말고도 다른 서비스 만들어야하는게 몬데여,,넹 아 그럼 준용님이 유저오더에대한 인포를 만드는 거고 맞습니다!
+//엔

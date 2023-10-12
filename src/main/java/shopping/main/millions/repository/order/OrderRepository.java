679d9b7aeb39fb3e5ab6001d70af293f8c0d56a7
dhdhk <hee0zzang@gmail.com>
2023-10-12 19:42:45 +0900

    feat : 오더프로덕트 검색 추가

--- src/main/java/shopping/main/millions/repository/order/OrderRepository.java
+++ src/main/java/shopping/main/millions/repository/order/OrderRepository.java
@@ -3,5 +3,8 @@ package shopping.main.millions.repository.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shopping.main.millions.entity.order.OrderEntity;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<OrderEntity,Long> {
+    List<OrderEntity> findByUserOrderEntity_UserOrderId(Long userOrderId);
 }

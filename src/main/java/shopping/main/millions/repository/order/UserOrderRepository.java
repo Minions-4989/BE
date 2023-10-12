37321452af8b03e47dc14f2fb65075fc1646633d
dhdhk <hee0zzang@gmail.com>
2023-10-12 19:43:50 +0900

    feat : 유저오더 검색 추가

--- src/main/java/shopping/main/millions/repository/order/UserOrderRepository.java
+++ src/main/java/shopping/main/millions/repository/order/UserOrderRepository.java
@@ -3,5 +3,8 @@ package shopping.main.millions.repository.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shopping.main.millions.entity.order.UserOrderEntity;
 
+import java.util.List;
+
 public interface UserOrderRepository extends JpaRepository<UserOrderEntity, Long> {
+    List<UserOrderEntity> findByCartEntity_CartId(Long cartId);
 }

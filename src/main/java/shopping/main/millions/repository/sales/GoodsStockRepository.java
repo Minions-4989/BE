34f229db4bdf17354e9b1a214fe94945c5103060
dhdhk <hee0zzang@gmail.com>
2023-10-12 21:35:44 +0900

    feat : 회원탈퇴시 판매물품목록 재고 0으로 추가

--- src/main/java/shopping/main/millions/repository/sales/GoodsStockRepository.java
+++ src/main/java/shopping/main/millions/repository/sales/GoodsStockRepository.java
@@ -1,11 +1,21 @@
 package shopping.main.millions.repository.sales;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import shopping.main.millions.entity.product.GoodsStockEntity;
 import shopping.main.millions.entity.product.ProductEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GoodsStockRepository extends JpaRepository<GoodsStockEntity,Long> {
     Optional<GoodsStockEntity> findByStockSizeAndStockColorAndProductEntity_ProductId (String size, String color, Long productId);
+    List<GoodsStockEntity> findAllByProductEntity_ProductId(Long productId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE GoodsStockEntity g SET g.stockQuantity = 0 WHERE g.productEntity.productId = :productId")
+    void updateStockQuantityToZeroByProductId(Long productId);
 }

d40846bbd40af63106f099ecf7cf8cce5b3ff538
dhdhk <hee0zzang@gmail.com>
2023-10-12 21:36:19 +0900

    feat : 회원탈퇴시 사용자활성화상태 false변경 추가

--- src/main/java/shopping/main/millions/repository/member/MemberRepository.java
+++ src/main/java/shopping/main/millions/repository/member/MemberRepository.java
@@ -1,10 +1,18 @@
 package shopping.main.millions.repository.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import shopping.main.millions.entity.member.MemberEntity;
 
 public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
     MemberEntity findByUserEmail(String email);
 
     MemberEntity findByTelNumber(String telNumber);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE MemberEntity m SET m.status = :status WHERE m.userId = :userId")
+    void updateMemberStatusByUserId(Long userId, boolean status);
 }

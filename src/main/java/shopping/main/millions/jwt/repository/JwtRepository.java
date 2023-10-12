276dacf918dab7ad4f6abfcafd6fad76aee8a0db
dhdhk <hee0zzang@gmail.com>
2023-10-12 21:36:01 +0900

    feat : 회원탈퇴시 리프레시 토큰 삭제 추가

--- src/main/java/shopping/main/millions/jwt/repository/JwtRepository.java
+++ src/main/java/shopping/main/millions/jwt/repository/JwtRepository.java
@@ -2,8 +2,14 @@ package shopping.main.millions.jwt.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import shopping.main.millions.jwt.entity.RefreshToken;
 
 public interface JwtRepository extends JpaRepository<RefreshToken, Long> {
-
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM RefreshToken r WHERE r.memberEntity.userId = :userId")
+    void deleteByMemberEntity_UserId(Long userId);
 }
